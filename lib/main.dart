8c39713dd91a574a324b08887a67a742c6d5a4e1
ashhad25 <ashhadahmed72@gmail.com>
2024-08-19 02:56:44 +0500

    added camera controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:test_ui/routes/routes.dart';
-import 'package:test_ui/utils/constants.dart';
+import 'package:test_ui/views/camera_screen.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(const MyApp());
 }
 
