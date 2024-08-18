98fe4107252680ef2ec188a1c952ad560522f3be
ashhad25 <ashhadahmed72@gmail.com>
2024-08-18 14:41:17 +0500

    color constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = const Color(0xFF5DC482);
+  static Color secondaryColor = const Color(0xFF364960);
+  static Color primarytextColor = const Color(0xFF141414);
+  static Color secondarytextColor = const Color(0xFFFFFFFF);
+  static Color cardBackgroundColor = const Color(0xFFECF3F6);
+}
